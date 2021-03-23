9505a8368eb0585771860d1fae571f4ef4a92a60
SebastianAlbrecht <77263753+SebastianAlbrecht@users.noreply.github.com>
2021-03-23 11:29:54 +0100

    Update stossprozesse_1d.py

--- stossprozesse_1d.py
+++ stossprozesse_1d.py
@@ -48,21 +48,43 @@ def free_to_interacting(free_trajectories, pos_boundary):
 
 # ---------- Animation Stoßprozesse ----------
     
-def anim_1D_colliding_particles(start_pos, start_vel):
+def anim_1D_colliding_particles(start_pos, start_vel, pos_boundary, time_step, num_steps, num_frames):
     
+    free_trajectories = free_propagation(start_pos, start_vel, time_step, num_steps)
+    interacting_trajectories = free_to_interacting(free_trajectories, pos_boundary)
+    
+    fig, ax = plt.subplots()
+    
+    
+    anim = animation.FuncAnimation(fig, update_colliding_particles_anim, interval=250, 
+                                   frames = num_frames, 
+                                   fargs = (interacting_trajectories, pos_boundary), 
+                                   repeat = False)
    
-    return
+    return anim
 
 
+def update_colliding_particles_anim(i, interacting_trajectories, pos_boundary):
+    
+    num_particles = interacting_trajectories.shape[1]
+    
+    fig = plt.gcf()
+    ax = fig.axes[0]
+    ax.clear()
+    ax.set_xlim(pos_boundary)
+    color_labels = np.linspace(1, num_particles, num = num_particles)
+    ax.scatter(interacting_trajectories[i], np.ones_like(interacting_trajectories[i]),
+               marker = "o", c = color_labels, cmap = "plasma")
+    
+    return ax
+      
 
-        
-
-# ----------  ----------
+# ---------- Hauptprogramm ----------
 
 pos_boundary = (1.,2.)
 num_particles = 5
-num_steps = 5 
-time_step = 0.5
+num_steps = 50
+time_step = 0.1
 
 
 start_pos = (pos_boundary[1] - pos_boundary[0]) * np.random.random(num_particles) + pos_boundary[0]
@@ -72,3 +94,7 @@ start_vel = np.random.random(num_particles) - 0.5
 free_trajectories = free_propagation(start_pos, start_vel, time_step, num_steps)
 interacting_trajectories = free_to_interacting(free_trajectories, pos_boundary)
 
+anim = anim_1D_colliding_particles(start_pos, start_vel, pos_boundary, time_step, num_steps, 50)
+
+writermp4 = animation.FFMpegWriter() 
+anim.save("name.mp4", writer=writermp4)
